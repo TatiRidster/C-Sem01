17e2efaee5d9538dbd7d4a8598651742e1ccff4e
Tatiana <tatnovid@gmail.com>
2022-04-28 17:23:15 +0300

    задача найти максимум двух чисел

---
+++ task00/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите два целых числа");
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+int numberB = Convert.ToInt32(Console.ReadLine());; 
+int Max;
+
+if (numberA > numberB)
+   {
+       Max = numberA;
+   }
+   else
+   {
+       Max = numberB;
+   }
+Console.WriteLine("Максимум из двух чисел");
+Console.WriteLine(Max);
