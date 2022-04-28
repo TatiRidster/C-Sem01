9b5bedd6e4e954a9e9e1ba4808b5af17e31f768e
Tatiana <tatnovid@gmail.com>
2022-04-28 21:09:59 +0300

    задача на четность числа

---
+++ task02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход
+//принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+
+Console.WriteLine("Введите целое число");
+double numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0)
+ {
+     Console.WriteLine("Ваше число четное");
+ }
+ else
+ {
+     Console.WriteLine("Ваше число нечетное");
+ }
