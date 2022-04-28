4f373e68c61d5c86923b9048db2f2a808ccd012b
Tatiana <tatnovid@gmail.com>
2022-04-28 20:37:16 +0300

    задача с максимумом трех чисел

---
+++ task01/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая принимает на
+//  вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три целых числа");
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberС = Convert.ToInt32(Console.ReadLine()); 
+int Max;
+bool x1 = (numberA > numberB);
+bool x2 = (numberA > numberС); 
+bool x3 = (numberB > numberС);
+
+if (x1 & x2 == true)
+   {
+       Max = numberA;
+   }
+   else if (x3==true)
+   {
+       Max = numberB; 
+   }
+   else 
+    {
+        Max=numberС;
+    }
+Console.WriteLine("Максимум из трех чисел");
+Console.WriteLine(Max);
